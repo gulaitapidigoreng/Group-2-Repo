b0f7d0fc9d9e49acb0bdc3cc118b408be20d8e6b
gulaitapidigoreng <162235442+gulaitapidigoreng@users.noreply.github.com>
2024-10-08 09:09:30 +0700

    Update TSP.py

--- TSP.py
+++ TSP.py
@@ -1,7 +1,7 @@
 # Function to parse the input
 def parse_input():
-    n = int(input())  # number of nodes
-    e = int(input())  # number of edges
+    n = int(input())  
+    e = int(input())
 
     # Initialize the distance matrix with large numbers (representing infinity)
     dist = [[float('inf')] * (n + 1) for _ in range(n + 1)]
@@ -14,9 +14,9 @@ def parse_input():
     for _ in range(e):
         edge_id, node1, node2, cost = map(int, input().split())
         dist[node1][node2] = cost
-        dist[node2][node1] = cost  # Since it's an undirected graph
+        dist[node2][node1] = cost
 
-    start_node = int(input())  # Start and end node for TSP
+    start_node = int(input())
 
     return n, dist, start_node
 
@@ -27,7 +27,7 @@ def initialize_memo(n):
 
 
 # TSP function (recursive with memoization)
-def tsp(i, mask, dist, memo, n):
+def tsp(i, mask, dist, memo, n, parent):
     # Base case: If all nodes are visited (except starting node)
     if mask == ((1 << i) | 3):
         return dist[1][i]
@@ -42,27 +42,58 @@ def tsp(i, mask, dist, memo, n):
     # Try all possible nodes to visit
     for j in range(1, n + 1):
         if (mask & (1 << j)) != 0 and j != i and j != 1:
-            res = min(res, tsp(j, mask & (~(1 << i)), dist, memo, n) + dist[j][i])
+            temp_res = tsp(j, mask & (~(1 << i)), dist, memo, n, parent) + dist[j][i]
+            if temp_res < res:
+                res = temp_res
+                parent[i][mask] = j
 
     memo[i][mask] = res
     return res
 
 
+# Function to reconstruct the route
+def find_route(parent, start, mask, n):
+    route = [start]
+    curr_node = start
+
+    while True:
+        next_node = parent[curr_node][mask]
+        if next_node is None:
+            break
+        route.append(next_node)
+        mask = mask & (~(1 << curr_node))
+        curr_node = next_node
+
+    return route
+
+
 # Main function to solve TSP
 def solve_tsp():
     n, dist, start_node = parse_input()
-    
-    # Initialize memoization table
+
+    # Initialize memoization and parent tables
     memo = initialize_memo(n)
-    
+    parent = [[None] * (1 << (n + 1)) for _ in range(n + 1)]
+
     # Find the minimum cost of the most efficient tour
     ans = 10**9
+    best_start = None
+
     for i in range(1, n + 1):
-        ans = min(ans, tsp(i, (1 << (n + 1)) - 1, dist, memo, n) + dist[i][1])
+        temp_ans = tsp(i, (1 << (n + 1)) - 1, dist, memo, n, parent) + dist[i][1]
+        if temp_ans < ans:
+            ans = temp_ans
+            best_start = i
+
+    # Reconstruct the route from the parent table
+    mask = (1 << (n + 1)) - 1
+    route = find_route(parent, best_start, mask, n)
+    route.append(1)
 
     # Output the result
-    print("The cost of most efficient tour = " + str(ans))
+    print(f"Cost = {ans}")
+    print(f"Route: {route}")
 
 
-# Call the function to run the TSP solver
+# Call the function
 solve_tsp()
